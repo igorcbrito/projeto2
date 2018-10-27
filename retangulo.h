559f14973006ee8591facbb8c575df9126ef0f5e
Malu <40304313+maluiza@users.noreply.github.com>
2018-10-27 17:40:55 -0300

    Create retangulo.h

---
+++ retangulo.h
@@ -0,0 +1,16 @@
+#ifndef RETANGULO_H
+#define RETANGULO_H
+#include "figurageometrica.h"
+#include "screen.h"
+
+class Retangulo: public FiguraGeometrica
+{
+private:
+    int x0, y0, l, a, fillmode;
+    char brush;
+public:
+	Retangulo(int _x0=0, int _y0=0, int _l=0, int _a=0, int _fillmode=0, char _brush='*');
+	void draw(Screen &t);
+};
+
+#endif //RETANGULO_H
