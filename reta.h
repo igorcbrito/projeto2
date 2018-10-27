7c16725a29e61be0652b5dd19311f13c9bbe91bb
Malu <40304313+maluiza@users.noreply.github.com>
2018-10-27 17:50:53 -0300

    Create reta.h

---
+++ reta.h
@@ -0,0 +1,16 @@
+#ifndef RETA_H
+#define RETA_H
+#include "figurageometrica.h"
+#include "screen.h"
+
+class Reta : public FiguraGeometrica {
+private:
+    int xi, yi, xf, yf;
+    char brush;
+public:
+    Reta(int _xi=0, int _yi=0, int _xf=0, int _yf=0,char _brush='*');
+    void draw(Screen &t);
+    int Sinal(int x);
+};
+
+#endif // RETA_H
