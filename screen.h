94b645db8a1e26bbc5777bde8b0eb30644245883
Malu <40304313+maluiza@users.noreply.github.com>
2018-10-29 08:48:49 -0300

    Update screen.h

--- screen.h
+++ screen.h
@@ -1,58 +1,14 @@
-#ifndef SCREEN_H
-#define SCREEN_H
-
-#include <iostream>
-#include <vector>
-#include <ostream>
-
 using namespace std;
-
-/**
- * @brief A classe Screen prove suporte para desenhar pontos em uma tela virtual. 
- Essa tela deverá ser implementada em uma matriz alocada dinamicamente conforme tamanho 
- a ser determinado pelo usuário.
- */
-class Screen
-{
+class Screen {
 private:
-    // armazenam o tamanho da tela de desenho
-    // no de linhas e de colunas
-    int nlin, ncol;
+    int nl, nc;
     char brush = '*';
-    // guarda o caractere usado para desenhar
-    vector< vector<char> > mat;
+    vector< vector<char> > matriz;
 public:
-    /**
-     * @brief Screen é o construtor da classe
-     * @param _nlin é o número de linhas da tela
-     * @param _ncol é o número de colunas da tela
-     */
-    Screen(int _nlin = 10, int _ncol = 10);
-
-    /**
-     * @brief setPixel desenha um pixel da matriz usando o caractere
-     * @param x é a coordenada x do pixel a ser alterado
-     * @param y é a coordenada y do pixel a ser alterado
-     */
+    Screen(int _nl = 10, int _nc = 10);
     void setPixel(int x, int y);
-
-    /**
-     * @brief clear limpa a tela
-     */
     void clear();
-
-    /**
-     * @brief setBrush muda o caractere de desenho
-     * @param _brush é o caracetere a ser usado para o pincel
-     */
     void setBrush(char _brush = '*');
-
-    /**
-     * @brief operator << é a sobrecarga do operador << 
-     * @param os é um parâmetro de fluxos de saída
-     * @param t é a tela na qual deve ser desenhada
-     * @return
-     */
     friend ostream& operator<<(ostream &os, Screen &t);
 };
 
