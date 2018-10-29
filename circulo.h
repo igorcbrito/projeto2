f72cf621468e2de81956f338f126f320be0b57a1
Malu <40304313+maluiza@users.noreply.github.com>
2018-10-29 17:45:37 -0300

    Create circulo.h

---
+++ circulo.h
@@ -0,0 +1,20 @@
+#ifndef CIRCULO_H
+#define CIRCULO_H
+#include "figurageometrica.h"
+#include "screen.h"
+
+class Circulo : public FiguraGeometrica {
+private:
+
+    int x0, y0, raio;
+    bool fillmode;
+    char brush;
+
+public:
+
+    Circulo(int _x0, int _y0, int _raio, bool _fillmode,char _brush='*');
+    void draw(Screen &t);
+    
+};
+
+#endif // CIRCULO_H
