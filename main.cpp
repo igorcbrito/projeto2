eb135a165370e2cfbbdafc35e58c0b2f7991bda5
Malu <40304313+maluiza@users.noreply.github.com>
2018-10-29 17:25:28 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include "screen.h"
+#include "retangulo.h"
+#include "reta.h"
+#include "circulo.h"
+#include <string>
+#include <sstream>
+#include <list>
+
+
+using namespace std;
+
+int main() {
+    ifstream fin;
+    ofstream fout;
+    Screen t;
+    string s;
+    fin.open("/home/malu/Área de Trabalho/Tratamento_Classes_Abstratas/instrucoes.txt");
+    fout.open("/home/malu/Área de Trabalho/Tratamento_Classes_Abstratas/figura.txt");
+    list <FiguraGeometrica*>::iterator li;
+    list <FiguraGeometrica*> figuras;
+    while(fin.good()){
+        getline(fin, s);
+        if(fin.good()){
+            string cmd;
+            stringstream sstream(s);
+            sstream >> cmd;
+            if(cmd.compare("dim")==0){
+                int x, y;
+                sstream >> x >> y;
+                t.setScreen(x,y);
+            }
+            if(cmd.compare("Reta")==0){
+                int x0, y0, xf,yf;
+                char brush;
+                sstream >> x0 >> y0 >> xf >> yf >> brush;
+                figuras.push_back(new Reta(x0,y0,xf,yf,brush));
+            }
+            if(cmd.compare("Retangulo")==0){
+                int x0, y0, largura,altura;
+                bool fillmode;
+                char brush;
+                sstream >> x0 >> y0 >> largura >> altura >> fillmode >> brush;
+                figuras.push_back(new Retangulo(x0,y0,largura,altura,fillmode,brush));
+            }
+            if(cmd.compare("Circulo")==0){
+                int x0, y0, raio;
+                bool fillmode;
+                char brush;
+                sstream >> x0 >>y0 >> raio >> fillmode >> brush;
+                figuras.push_back(new Circulo(x0,y0,raio,fillmode,brush));
+            }
+        }
+    }
+    for(li = figuras.begin(); li != figuras.end(); li++){
+        (*li)->draw(t);
+      }
+    fout << t;
+    return 0;
+}
+
