8c533e95065c157439635027070b1c93089675d9
Malu <40304313+maluiza@users.noreply.github.com>
2018-10-30 14:38:50 -0300

    Update reta.h

--- reta.h
+++ reta.h
@@ -5,12 +5,16 @@
 
 class Reta : public FiguraGeometrica {
 private:
+
     int xi, yi, xf, yf;
     char brush;
+
 public:
-    Reta(int _xi=0, int _yi=0, int _xf=0, int _yf=0,char _brush='*');
+
+    Reta(int _xi = 0, int _yi = 0, int _xf = 0, int _yf = 0,char _brush = '*');
     void draw(Screen &t);
     int Sinal(int x);
+    
 };
 
 #endif // RETA_H
